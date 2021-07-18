19b92d50ee36855341a55d7ba6648938c22f238d
tdf1939 <tdf261939@gmail.com>
2021-07-18 12:17:17 +0800

    main

--- img/img.go
+++ img/img.go
@@ -1,11 +1,49 @@
 package img
 
-import "github.com/fogleman/gg"
+import (
+	"image"
+	"math"
+
+	"github.com/fogleman/gg"
+)
+
+// 加载图片，path为图片路径
+func ImgPath(path string) (im image.Image) {
+	im, _ = gg.LoadImage(path)
+	return
+}
+
+// 获取高，宽
+func Wh(i image.Image) (w, h int) {
+	w = i.Bounds().Size().X
+	h = i.Bounds().Size().Y
+	return
+}
 
 func Out() {
 	dc := gg.NewContext(1000, 1000)
 	dc.DrawCircle(500, 500, 400)
 	dc.SetRGB(0, 0, 0)
 	dc.Fill()
+	dc.SavePNG("out.png")
+}
+
+func Yuan() {
+	im := ImgPath("cs\\1.png")
+
+	// 获取图片的宽度和高度
+	w := im.Bounds().Size().X
+	h := im.Bounds().Size().Y
+
+	dc := gg.NewContext(h, w)
+
+	// 取宽度和高度的最小值作为直径
+	radius := math.Min(float64(w), float64(h)) / 2
+	// 画圆形
+	dc.DrawCircle(float64(w/2), float64(h/2), radius)
+	// 对画布进行裁剪
+	dc.Clip()
+	// 加载图片
+	dc.DrawImage(im, 0, 0)
 	dc.SavePNG("out.png")
 }
--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "github.com/tdf1939/img/img"
+import (
+	"fmt"
+
+	"github.com/tdf1939/img/img"
+)
 
 func main() {
-	img.Out()
+	//加载图片
+	im := img.ImgPath("D:\\github\\sucai\\cs\\1.png")
+	//获取宽高
+	w, h := img.Wh(im)
+	fmt.Printf("%d %d\n", w, h)
+
 }
