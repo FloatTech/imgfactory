ad1e799d67dc1120c6953ef7c3d42319b5ba80c3
散无友纪 <tdf261939@gmail.com>
2021-08-10 23:21:32 +0800

    Update main.go

--- example/main.go
+++ example/main.go
@@ -60,7 +60,7 @@ func main() {
 	})
 
 	// 帮助
-	zero.OnFullMatchGroup([]string{"help", "/help", ".help", "菜单", "帮助"}, zero.OnlyToMe).SetBlock(true).SetPriority(999).
+	zero.OnFullMatchGroup([]string{"help", "/help", ".help", "菜单", "帮助"}, zero.OnlyToMe).SetBlock(false).SetPriority(999).
 		Handle(func(ctx *zero.Ctx) {
 			ctx.Send(banner)
 		})
